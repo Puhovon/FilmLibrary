d8963787684b9a9ab072fb5c58d6ec1d4246a57f
Puhovon <91030077+Puhovon@users.noreply.github.com>
2023-03-05 16:12:04 +0300

    Добавьте файлы проекта.

---
+++ FilmLibrary/Model/Film.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLibrary.Model
+{
+    internal class Film
+    {
+        public string FilmName
+        {
+            get;
+            set;
+        }
+        public string Country
+        {
+            get; set;
+        }
+        public string Director
+        {
+            get; set;
+        }
+        public string Genre
+        {
+            get; set;
+        }
+    }
+}
