5f1ba0e5e14e5a4f025036efd3257affe34e4adf
Puhovon <91030077+Puhovon@users.noreply.github.com>
2023-03-15 19:01:02 +0300

    Добавление команды для закрытия приложения

--- FilmLibrary/Infrastructure/Commands/LambdaCommand.cs
+++ FilmLibrary/Infrastructure/Commands/LambdaCommand.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FilmLibrary.Infrastructure.Commands.Base;
 
 namespace FilmLibrary.Infrastructure.Commands
 {
-    class LambdaCommand
+    class LambdaCommand : Command
     {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+        public override bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+
+        public override void Execute(object? parameter) => _execute(parameter);
     }
 }
--- FilmLibrary/ViewModel/MainVindowViewModel.cs
+++ FilmLibrary/ViewModel/MainVindowViewModel.cs
@@ -1,24 +1,52 @@
 ﻿using FilmLibrary.Model;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using FilmLibrary.Infrastructure.Commands;
 using FilmLibrary.ViewModel.Base;
 
 namespace FilmLibrary.ViewModel
 {
     internal class MainVindowViewModel : ViewModelBase
     {
+        #region Title
         private string _title = "HomeWork17";
         public string Title
         {
-            get=>_title;
+            get => _title;
             set
             {
                 Title = value;
             }
         }
+        #endregion
 
+        #region Commands
+
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        #endregion
+        
+        
+        #endregion
         public MainVindowViewModel()
         {
+            #region Commands
 
+            CloseApplicationCommand =
+                new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
         }
     }
 }
